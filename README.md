f2b60f24a910f2713d0bc8eee6489f43b64ce4f2
Max Milan <maxmilan10@hotmail.com>
2026-02-12 14:53:24 -0300

    feat: using heroUI initial config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ro-mvp-hunter
+
 Uma ferramenta para facilitar a caçada de MVP's em Ragnarok Online, especialmente para o servidor History Reborn
---
+++ src/config/site.ts
@@ -0,0 +1,46 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "⚔️ MVP Tracker",
+  description: "Track your MVPs and get notified when they are ready.",
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+  },
+};
